20738ebedafefa0bfb49e96fb9336c76d79f4023
xurik <sunday.xuri@gmail.com>
2012-12-06 22:43:03 +0800

    t

---
+++ src/test/java/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sunday
+ * Date: 12/6/12
+ * Time: 10:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test{
+}
